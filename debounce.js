1e1511a5d214419ff46355f8b449ce7706639df4
maiyonnaise <dxuanmai43@gmail.com>
2022-09-20 02:08:52 +0700

    second fix

--- debounce.js
+++ debounce.js
@@ -1,19 +1,43 @@
-let debouncedTimer ;
 
-function debounce(callback, time){
-    clearTimeout(debouncedTimer); 
-    debouncedTimer = setTimeout(callback, time);
-};
+const debounceCount = document.querySelector('.debounce-count');
+const throttleCount = document.querySelector('.throttle-count');
+const input = document.querySelector('.input');
+const inputValue = document.querySelector('.input-value');
 
-let throttlePause;
-function throttle(callback, time){
-    if (throttlePause){
-        return ;
+
+
+function debounce(callback, time) {
+    let debounceTimer;
+    return (...args) => {
+        clearTimeout(debounceTimer);
+        debounceTimer = setTimeout(() => {
+            callback(...args);
+        }, time);
     };
-    throttlePause = true;
-    setTimeout(()=> {
-        callback();
-        throttlePause = false;
-    }, time);
 }
 
+const updateDebounce = debounce((input) => {
+    debounceCount.textContent = input;
+}, 500);
+
+const updateThrottle = throttle((input) => {
+    throttleCount.textContent = input;
+}, 1000);
+function throttle(callback, time) {
+    let throttlePause = 0;
+    return (...args) => {
+        let now = new Date().getTime();
+        if (now - throttlePause < time) {
+            return;
+        }
+        throttlePause = now;
+        return callback(...args);
+    };
+}
+
+
+input.addEventListener('input', (e) => {
+    inputValue.textContent = input.value;
+    updateDebounce(e.target.value);
+    updateThrottle(e.target.value);
+});
